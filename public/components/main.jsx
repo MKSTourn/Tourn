2acf663b07ea21698e44760d9e2961307922e991
Zak <legobug2@hotmail.com>
2016-06-08 13:44:20 -0500

    Wrote script to run eslint on project. Added deep-freeze to developer (test) dependencies, changed main.jsx to conform to Airbnb style guide

--- public/components/main.jsx
+++ public/components/main.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class MainComponent extends React.Component {
-	constructor() {
-		super();
-	}
+/* class MainComponent extends React.Component {
+  constructor() {
+    super();
+  }
 
-	render() {
-		return (<div>I am a test.</div>);
-	}
-}
+  render() {
+    return (<div>I am a test.</div>);
+  }
+} */
+
+const MainComponent = () => <div> I am a test. </div>;
 
 ReactDOM.render(
-	<MainComponent />,
-	document.getElementById('entrypoint')
+  <MainComponent />,
+  document.getElementById('entrypoint')
 );
