72f3cf502e468f73dd44484bb8430e67f6cbfa57
SpartanSOS <markb539@gmail.com>
2016-06-10 14:56:34 -0500

    Implemented first test

--- public/reducers/rules.jsx
+++ public/reducers/rules.jsx
@@ -1,4 +1,4 @@
-const rules = (state = [], action) => {
+const rules = (state, action) => {
  let resultState = null;
  let tournamentMorph = null;
 
@@ -14,17 +14,7 @@ const rules = (state = [], action) => {
          },
        });
        return resultState;
-
-     case 'Scoring_System':
-
-
-
-     default:
-       return state;
-   }
- }
 return state;
 };
-
 
 export default rules;
