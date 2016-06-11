ca3f509ae98cc140be91e42890918e4226571a9a
Mark Boraski <SpartanSOS@users.noreply.github.com>
2016-06-11 11:12:08 -0500

    Testing setup (#10)
    * Add folders
    
    * delete app.jsx
    
    * Start implementing testing
    
    * Implemented first test
    
    * Future reducer changes coming

---
+++ public/reducers/rules.jsx
@@ -0,0 +1,30 @@
+const rules = (state = [], action) => {
+ let resultState = null;
+ let tournamentMorph = null;
+
+ if (action.type === 'CHANGE_RULE') {
+   switch (action.rule) {
+     case 'Tournament_Type':
+       resultState = Object.assign({}, state);
+       tournamentMorph = Object.assign({}, state.tournament);
+       resultState.tournament = Object.assign(tournamentMorph, {
+         rules: {
+           rule: action.rule,
+           value: action.value,
+         },
+       });
+       return resultState;
+
+     case 'Scoring_System':
+      
+
+
+     default:
+       return state;
+   }
+ }
+return state;
+};
+
+
+export default rules;
