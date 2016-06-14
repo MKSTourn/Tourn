4ab4cbebd291c87769c0b4c49483bf057b252729
Zackery P <UndeadTwo@users.noreply.github.com>
2016-06-14 10:55:05 -0500

    Bracket Feature and Changes to header for Style guide compliance (#16)
    * Got React working, changed main.jsx to fix name errors.
    
    * Fixed eslint errors and added React-Redux and Redux to dependencies
    
    * Wrote script to run eslint on project. Added deep-freeze to developer (test) dependencies, changed main.jsx to conform to Airbnb style guide
    
    * Added dependencies
    
    * Created utility file for bracket generation
    
    * Implemented rendering logic for backing single elimanation bracket flow
    
    * Significant changes to HeaderComponent for compliance with AirBNB style guide

--- public/components/main.jsx
+++ public/components/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-var Header = require('./Header/HeaderComponent.jsx');
+import { HeaderComponent } from './Header/HeaderComponent.jsx';
+import { Bracket } from './bracket.jsx';
+
 
 /* class MainComponent extends React.Component {
   constructor() {
@@ -12,12 +14,15 @@ var Header = require('./Header/HeaderComponent.jsx');
   }
 } */
 
-const MainComponent = () => <div> 
-  <Header />
- </div>;
+
+// console.log(Bracket);
+
+const MainComponent = () => (<div>
+  <HeaderComponent />
+  <Bracket />
+</div>);
 
 ReactDOM.render(
   <MainComponent />,
-  
   document.getElementById('entrypoint')
 );
