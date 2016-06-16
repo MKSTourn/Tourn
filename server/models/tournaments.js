e3df12e31377ab805ebce0425be71ac9265c8e43
Zak <legobug2@hotmail.com>
2016-06-16 15:30:30 -0500

    Database tournmanet model functions

--- server/models/tournaments.js
+++ server/models/tournaments.js
@@ -6,5 +6,19 @@ Tournaments.create = () => new Promise((resolve, reject) => {
   TournamentSchema.create({}, (err, result) => {
     if (err) reject(err);
     resolve(result);
-  })
+  });
+});
+
+Tournaments.findById = (tournamentid) => new Promise((resolve, reject) => {
+  TournamentSchema.findById(tournamentid, (err, result) => {
+    if (err) reject(err);
+    resolve(result);
+  });
+});
+
+Tournaments.findByUser = (userid) => new Promise((resolve, reject) => {
+  TournamentSchema.find({ roster: { playerId: userid } }, (err, result) => {
+    if (err) reject(err);
+    resolve(result);
+  });
 });
