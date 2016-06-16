4db032007223989ab3ddd036d3e8c36efc4b97b9
SpartanSOS <markb539@gmail.com>
2016-06-16 17:21:29 -0500

    Implemented client chat message generation, bootstrap for react, refactor index.html and main to follow current architecture

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HeaderComponent } from './Header.jsx';
 import { Bracket } from './Bracket.jsx';
-
-
+import { Chat } from './Chat.jsx';
+import { Grid, Row, Col } from 'react-bootstrap';
+import { io } from 'socket.io';
 /* class MainComponent extends React.Component {
   constructor() {
     super();
@@ -17,12 +18,51 @@ import { Bracket } from './Bracket.jsx';
 
 // console.log(Bracket);
 
-const MainComponent = () => (<div>
-  <HeaderComponent />
-  <Bracket />
-</div>);
+// let handleMessageSubmit = (message) => (
+//   //TODO: submit to the server and refresh the list
+//   console.log('handleMessageSubmit ran')
+// )
+
+const MainComponent = () => {
+  let socket = io();
+  handleMessageSubmit: function(author = 'Mark', text){
+    let timeStamp = Date.now();
+    socket.emit('chat message', {author: author, text: text, time: timeStamp});
+  }
+
+  return (
+  <Grid>
+    <Row className='show-grid'>
+      <Col md={12}>
+        <HeaderComponent />
+      </Col>
+    </Row>
+    <Row className='show-grid'>
+      <Col md={3}>
+        <div id="roster"></div>
+      </Col>
+      <Col md={6}>
+        <Bracket />
+      </Col>
+      <Col md={3}>  
+        <Chat onMessageSubmit={this.handleMessageSubmit} />
+      </Col>
+    </Row>
+    <Row>
+      <Col md={3}>
+        <span></span>
+      </Col>
+      <Col md={6}>
+        <div id="rules"></div>
+      </Col>
+      <Col md={3}>  
+        <span></span>
+      </Col>
+    </Row>
+  </Grid>)
+};
 
 ReactDOM.render(
   <MainComponent />,
-  document.getElementById('header')
+  document.getElementById('app')
 );
