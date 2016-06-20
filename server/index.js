e4bfc1b01f52140195d6e023f13595df4593bd1c
Zackery P <UndeadTwo@users.noreply.github.com>
2016-06-20 10:29:50 -0500

    Database, Facebook, Chat.jsx was changed to not crash babel, Express sessions are saved to mongodb. (#41)
    * Got React working, changed main.jsx to fix name errors.
    
    * Fixed eslint errors and added React-Redux and Redux to dependencies
    
    * Created utility file for bracket generation
    
    * Implemented rendering logic for backing single elimanation bracket flow
    
    * Demonstration changes
    
    * Database models
    
    * Database tournmanet model functions
    
    * Incomplete database/auth implementation
    
    * Refactored to move authentication to a seperate route folder
    
    * Chat.jsx was made syntactically correct

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
+// Initialize our database connection
+require('./database-config.js');
+
+// Initialize our webserver
 const express = require('express');
+
+// Request monitoring middleware
 const morgan = require('morgan');
+
+// Client source bundle generator
 const webpack = require('webpack');
 const webpackMiddleWare = require('webpack-dev-middleware');
 const webpackConfig = require('../webpack.config');
+
 // Setup web server
 const app = express();
 
@@ -21,106 +30,16 @@ app.use(webpackMiddleWare(config, {}));
 // Attach static assets to /static/ endpoint
 app.use('/', express.static(`${__dirname}/../client/public/`));
 
-// Simple hello world message to test server configuration
+app.get('/', (req, res) => {
+  res.redirect('/index.html');
+});
 
 // Start listening to a port defined by the environment or 4000
 const port = process.env.PORT || 4000;
 console.log(`Listening on port: ${port}`);
 
-
-//***Facebook Stuff For Now***//
-
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var cookieSession = require('cookie-session');
-var session = require('express-session');
-var uuid = require('uuid-v4');
-var passport = require('passport'),
- FacebookStrategy = require('passport-facebook').Strategy;
-//
-// creates unique UUID for session
-//
-app.use(session({
-  genid: function(req) {
-    return uuid(); 
-  },
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { maxAge: 100000 }
-}));
-app.use(cookieParser());
-app.use(passport.initialize());
-app.use(passport.session());
-app.use('/auth/', bodyParser.json());
-//
-// serialize and deserialize session token
-//
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
-
-
-//***************Facebook Authentication Routing********************
-passport.use(new FacebookStrategy({
-    clientID: '',
-    clientSecret: '',
-    callbackURL: "http://localhost:4000/auth/facebook/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-    var _id = profile.id;
-    console.log("accessToken:", accessToken);
-    // console.log("refreshToken:", refreshToken);
-    console.log("profile:", profile.displayName);
-    // console.log("done:", done);
-    console.log("id:", _id);
-
-    //
-    // Functions below are exampleS of calling server models here.
-    //
-
-    // User.findById(_id)
-    // .then(function(response){
-    //   console.log("userRes:", response);
-    // })
-    // .catch(function(error){
-    //   console.error("userResErr: ", error);
-    // })
-    //
-    // User.create(_id, profile.displayName)
-    // .then(function(user){
-    //   console.log("newUserRes:", user);
-    //   return done(null, user);
-    // })
-    // .catch(function(error){
-    //   return done(error);
-    // })
-    return done(null, 'login worked');
-  }
-));
-
-// Redirect the user to Facebook for authentication.  When complete,
-// Facebook will redirect the user back to the application at
-//     /auth/facebook/callback
-app.get('/auth/facebook', passport.authenticate('facebook'), function(req, res){ 
-	// available callback function
-});
-
-// Facebook will redirect the user to this URL after approval.  Finish the
-// authentication process by attempting to obtain an access token.  If
-// access was granted, the user will be logged in.  Otherwise,
-// authentication has failed.
-app.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { 
-  	failureRedirect: '/login' 
-  }),
-  function(req, res){
-    res.redirect('/');
-  }
-);
-//******************************************************************
+// Attach our authorization module to the app.
+const auth = require('./routes/auth.js');
+auth(app);
 
 app.listen(port);
---
+++ server/schemas/tournaments.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const tournamentSchema = new Schema({
+  organizerid: Schema.ObjectId,
+
+  name: String,
+
+  type: String,
+
+  // All messages for a given tournament
+  chatHistory: [{
+    sender: Schema.ObjectId,
+    message: String,
+  }],
+
+  rules: String,
+
+  // All players regardless of current status
+  roster: [{
+    playerId: Schema.ObjectId,
+  }],
+
+  // All current and previous rounds
+  bracket: [{
+    round: [{
+      playerA: Schema.ObjectId,
+      playerB: Schema.ObjectId,
+      winner: Schema.ObjectId,
+    }],
+  }],
+});
+
+module.exports = mongoose.model('Tournament', tournamentSchema);
