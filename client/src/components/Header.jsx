5541acddea6f93860aa135736237011ccba88b04
Zak <legobug2@hotmail.com>
2016-06-21 14:36:37 -0500

    Solved the mysterious case of the auto exectuing function in the Header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,6 +4,30 @@ import * as actions from '../actions/action_creators.jsx';
 import { connect } from 'react-redux';
 
 function Header(props) {
+  if (props.mode === 'LoggedOut') {
+    return (<div className="topbar">
+      <h1 className="center">Tourn</h1>
+      <button onClick={props.changeMode} className="facebookLogin">Login</button>
+      <a onClick={props.changeMode('LoggedIn')} href="auth/facebook">Login with facebook</a>
+    </div>);
+  } else if (props.mode === 'Edit') {
+    return (<div className="topbar">
+      <button onClick={props.changeMode} className="addTournamentButton">
+        <span className="symbol">+</span>
+      </button>
+      <button onClick={props.changeMode} className="submitTournamentButton">
+        <span className="symbol">√</span>
+      </button>
+      <button onClick={props.changeMode} className="dropdownBtn">
+       My Tournaments
+      </button>
+      <h1 className="center">Tourn</h1>
+      <button onClick={props.changeMode('LoggedOut')} className="alertBtn">
+       Alerts
+      </button>
+    </div>);
+  }
+
   return (<div className="topbar">
     <button onClick={props.changeMode} className="addTournamentButton">
       <span className="symbol">+</span>
@@ -15,7 +39,7 @@ function Header(props) {
      My Tournaments
     </button>
     <h1 className="center">Tourn</h1>
-    <button onClick={props.changeLoggedOut} className="alertBtn">
+    <button onClick={props.changeMode('LoggedOut')} className="alertBtn">
      Alerts
     </button>
   </div>);
