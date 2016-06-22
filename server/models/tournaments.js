2dbff11cd1128a9e29169ef28bc96998e3c6dc91
Zak <legobug2@hotmail.com>
2016-06-22 11:42:38 -0500

    Significant changes to users and tournaments model. Sockets now interact with database, user schema changed to remove sessions, as they are stored in a seperate collection and not directly controlled by our application.

--- server/models/tournaments.js
+++ server/models/tournaments.js
@@ -2,8 +2,16 @@ const TournamentSchema = require('../schemas/tournaments.js');
 
 const Tournaments = module.exports;
 
-Tournaments.create = () => new Promise((resolve, reject) => {
-  TournamentSchema.create({}, (err, result) => {
+Tournaments.create = (organizerid, name, type, rules) => new Promise((resolve, reject) => {
+  TournamentSchema.create({
+    organizerid,
+    name,
+    type,
+    rules,
+    roster: [{
+      playerId: organizerid,
+    }],
+  }, (err, result) => {
     if (err) reject(err);
     resolve(result);
   });
@@ -20,5 +28,49 @@ Tournaments.findByUser = (userid) => new Promise((resolve, reject) => {
   TournamentSchema.find({ roster: { playerId: userid } }, (err, result) => {
     if (err) reject(err);
     resolve(result);
+  });
+});
+
+Tournaments.addChatMessage = (tournid, sender, message) => new Promise((resolve, reject) => {
+  TournamentSchema.findById(tournid, (err, result) => {
+    if (err) reject(err);
+    result.chatHistory.push({ sender, message });
+    result.save((saveErr, saveResult) => {
+      if (saveErr) reject(saveErr);
+      resolve(saveResult);
+    });
+  });
+});
+
+Tournaments.addRosterPlayer = (tournid, playerId) => new Promise((resolve, reject) => {
+  TournamentSchema.findById(tournid, (err, result) => {
+    if (err) reject(err);
+    result.roster.push({ playerId });
+    result.save((saveErr, saveResult) => {
+      if (saveErr) reject(saveErr);
+      resolve(saveResult);
+    });
+  });
+});
+
+Tournaments.advancePlayer = (tournid, playerId, match) => new Promise((resolve, reject) => {
+  TournamentSchema.findById(tournid, (err, result) => {
+    if (err) reject(err);
+
+    const res = Math.floor(match / 2);
+    const someFurtherMatch = (result.roster.length / 2) + res;
+
+    result.bracket[match].winner = playerId;
+    if(!result.bracket[someFurtherMatch]) {
+      result.bracket[someFurtherMatch] = {};
+      result.bracket[someFurtherMatch].playerA = playerId;
+    } else {
+      result.bracket[someFurtherMatch].playerB = playerId;
+    }
+
+    result.save((saveErr, saveResult) => {
+      if (saveErr) reject(saveErr);
+      resolve(saveResult);
+    });
   });
 });
--- server/models/users.js
+++ server/models/users.js
@@ -13,22 +13,6 @@ Users.create = (name, fbid) => new Promise((resolve, reject) => {
   });
 });
 
-Users.createSession = (userid) => new Promise((resolve, reject) => {
-  Users.findById(userid)
-    .then((result) => {
-      const token = uuid.v4();
-      result.sessions.push({ token });
-      result.save((err) => {
-        if (err) reject(err);
-        resolve(token);
-      });
-    })
-    .catch((err) => {
-      console.error('Error while creating user session, ', err);
-      reject(err);
-    });
-});
-
 // Search functions
 
 Users.findById = (userid) => new Promise((resolve, reject) => {
