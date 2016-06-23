801fb6c2b397356dbc96fe2b680c526645b13982
Maher Dakkak <madakkak@gmail.com>
2016-06-23 16:20:49 -0500

    Add tournName property in userData's alert object. To be a true template, comment out all default array elements so we can see the necessary object shape but still initialize to empty arrays. Implement header reducer logic to calculate next states. Not tested yet.

--- client/src/reducers/header.jsx
+++ client/src/reducers/header.jsx
@@ -2,17 +2,17 @@
 // Header reducer
 //
 
-import INITIAL_STATE from '../../../data/state.jsx';
+// import INITIAL_STATE from '../../../data/state.jsx';
 import { fromJS } from 'immutable';
 
-function handleSubmit(state, tourn) {
+function handleSubmit(state) {
   console.log('Header reducer: handleSubmit');
   // User's new tourn info has been submitted to server.
   // Nothing to render in the meantime, so don't change state.
   return state;
 }
 
-function handleSelectTourn(state, tournId) {
+function handleSelectTourn(state) {
   console.log('Header reducer: handleSelectTourn');
   // User's new tourn info has been submitted to server.
   // Nothing to render in the meantime, so don't change state.
@@ -23,47 +23,60 @@ function handleDeleteAlert(state, alertId) {
   console.log('Header reducer: handleDeleteAlert');
   // Optimistic next state:
   // Delete alertId obj from userData.alerts
-  const alerts = state.get('alerts').toJS();
+  let alerts = state.get('alerts').toJS();
   const delIndex = alerts.findIndex(alert => alert.alertId === alertId);
+  alerts = alerts.splice(delIndex, 1);
   return ~delIndex ?
-             state : state.set('alerts', fromJS(alerts.splice(delIndex, 1)));
+             state : state.set('alerts', fromJS(alerts));
 }
 
 function handleAcceptInvite(state, tournId, alertId) {
   console.log('Header reducer: handleAcceptInvite');
   // Optimistic next state:
   // Add tournId to userData.userTourns
-  return state;
+  let alerts = state.get('alerts').toJS();
+  let tourns = state.get('userTourns').toJS();
+  const delIndex = alerts.findIndex(alert => alert.alertId === alertId);
+  const newTourn = {
+    tournName: alerts[delIndex].tournName,
+    tournId,
+  };
+
+  tourns = tourns.splice(0, 0, newTourn);
+  alerts = alerts.splice(delIndex, 1);
+
+  return state.set('alerts', fromJS(alerts))
+              .set('userTourns', fromJS(tourns));
 }
 
-function handleToggleSelect(state, showTournList) {
-  let toggledTournList = !state.get('showTournList');
-  console.log('handleToggleSelect reducer function ran');
+function handleToggleSelect(state) {
+  const toggledTournList = !state.get('showTournList');
+  console.log('Header reducer: handleToggleSelect');
   console.log('state before update', state);
-  return state.set('showTournList', toggledTournList );
+  return state.set('showTournList', toggledTournList);
 }
 
-function handleToggleAlerts(state, showAlertList) {
-  let toggleAlertList = !state.get('showAlertList');
-  console.log('handleToggleAlerts reducer function ran');
-  console.log('state before update', state);
-  return state.set('showAlertList', toggleAlertList);
+function handleToggleAlerts(state) {
+  const toggledAlertList = !state.get('showAlertList');
+
+  console.log('Header reducer: handleToggleAlerts');
+  return state.set('showAlertList', toggledAlertList);
 }
 
 export default function header(state, action) {
   switch (action.type) {
     case 'SUBMIT_NEW_TOURN':
-      return handleSubmit(state, action.tourn);
+      return handleSubmit(state);
     case 'SELECT_TOURN':
-      return handleSelectTourn(state, action.tournId);
+      return handleSelectTourn(state);
     case 'DELETE_ALERT':
       return handleDeleteAlert(state, action.userId, action.alert);
     case 'ACCEPT_INVITE':
       return handleAcceptInvite(state, action.userId, action.tournId, alert);
     case 'TOGGLE_SELECT':
-      return handleToggleSelect(state, action.showTournList); //CHANGED!!!
+      return handleToggleSelect(state);
     case 'TOGGLE_ALERTS':
-      return handleToggleAlerts(state, action.showAlertList);
+      return handleToggleAlerts(state);
     default:
       return state;
   }
