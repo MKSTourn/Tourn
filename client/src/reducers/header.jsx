e27e6002022f73e0658b0b424202b371be0afd7c
Maher Dakkak <madakkak@gmail.com>
2016-06-23 19:20:19 -0500

    Implement initial, next states and full reducer tests for every header reducer function. Fix all bugs in header reducer functions so next state is correctly calculated. Fix a bug in action creators where both toggle tourn select and toggle alert select were firing the TOGGLE_SELECT action.

--- client/src/reducers/header.jsx
+++ client/src/reducers/header.jsx
@@ -3,7 +3,7 @@
 //
 
 // import INITIAL_STATE from '../../../data/state.jsx';
-import { fromJS } from 'immutable';
+import { fromJS, toJS } from 'immutable';
 
 function handleSubmit(state) {
   console.log('Header reducer: handleSubmit');
@@ -16,49 +16,56 @@ function handleSelectTourn(state) {
   console.log('Header reducer: handleSelectTourn');
   // User's new tourn info has been submitted to server.
   // Nothing to render in the meantime, so don't change state.
+  // TODO: Change the currently selected tourn name to the name
+  // of the newly selected one (even though the server hasn't
+  // given us the tournament state yet)
   return state;
 }
 
 function handleDeleteAlert(state, alertId) {
   console.log('Header reducer: handleDeleteAlert');
+  console.log('Header reducer: alertId =', alertId);
+  console.log('Header reducer: state =', state);
   // Optimistic next state:
   // Delete alertId obj from userData.alerts
-  let alerts = state.get('alerts').toJS();
+  const alerts = state.getIn(['userData', 'alerts']).toJS();
+  console.log('Header reducer: alerts =', alerts);
   const delIndex = alerts.findIndex(alert => alert.alertId === alertId);
-  alerts = alerts.splice(delIndex, 1);
-  return ~delIndex ?
-             state : state.set('alerts', fromJS(alerts));
+  alerts.splice(delIndex, 1);
+  console.log('Header reducer: alerts after delete =', alerts);
+  return ~delIndex ? state.setIn(['userData', 'alerts'], fromJS(alerts)) :
+                     state;
 }
 
 function handleAcceptInvite(state, tournId, alertId) {
   console.log('Header reducer: handleAcceptInvite');
   // Optimistic next state:
-  // Add tournId to userData.userTourns
-  let alerts = state.get('alerts').toJS();
-  let tourns = state.get('userTourns').toJS();
+  // Add new tourn to userData.userTourns
+  const alerts = state.getIn(['userData', 'alerts']).toJS();
+  let tourns = state.getIn(['userData', 'userTourns']).toJS();
   const delIndex = alerts.findIndex(alert => alert.alertId === alertId);
+  console.log('Header reducer: deleteIndex =', delIndex);
   const newTourn = {
     tournName: alerts[delIndex].tournName,
     tournId,
   };
 
-  tourns = tourns.splice(0, 0, newTourn);
-  alerts = alerts.splice(delIndex, 1);
+  tourns.splice(0, 0, newTourn);
+  alerts.splice(delIndex, 1);
 
-  return state.set('alerts', fromJS(alerts))
-              .set('userTourns', fromJS(tourns));
+  return ~delIndex ? state.setIn(['userData', 'alerts'], fromJS(alerts))
+                          .setIn(['userData', 'userTourns'], fromJS(tourns)) :
+                     state.setIn(['userData', 'userTourns'], fromJS(tourns));
 }
 
 function handleToggleSelect(state) {
   const toggledTournList = !state.get('showTournList');
   console.log('Header reducer: handleToggleSelect');
-  console.log('state before update', state);
   return state.set('showTournList', toggledTournList);
 }
 
 function handleToggleAlerts(state) {
   const toggledAlertList = !state.get('showAlertList');
-
   console.log('Header reducer: handleToggleAlerts');
   return state.set('showAlertList', toggledAlertList);
 }
@@ -70,9 +77,9 @@ export default function header(state, action) {
     case 'SELECT_TOURN':
       return handleSelectTourn(state);
     case 'DELETE_ALERT':
-      return handleDeleteAlert(state, action.userId, action.alert);
+      return handleDeleteAlert(state, action.alertId);
     case 'ACCEPT_INVITE':
-      return handleAcceptInvite(state, action.userId, action.tournId, alert);
+      return handleAcceptInvite(state, action.tournId, action.alertId);
     case 'TOGGLE_SELECT':
       return handleToggleSelect(state);
     case 'TOGGLE_ALERTS':
--- test/client/header_spec.jsx
+++ test/client/header_spec.jsx
@@ -5,7 +5,16 @@ import { describe, it } from 'mocha';
 import * as actions from '../../client/src/actions/action_creators.jsx';
 import header from '../../client/src/reducers/header.jsx';
 import {
-
+        SUBMIT_STATE,
+        SUBMIT_STATE_NEXT,
+        DELETE_ALERT_STATE,
+        DELETE_ALERT_STATE_NEXT,
+        ACCEPT_INVITE_STATE,
+        ACCEPT_INVITE_STATE_NEXT,
+        TOGGLE_ALERT_STATE,
+        TOGGLE_ALERT_STATE_NEXT,
+        TOGGLE_SELECT_STATE,
+        TOGGLE_SELECT_STATE_NEXT,
        } from './states/header_spec_states.jsx';
 
 chai.use(chaiImmutable);
@@ -13,60 +22,60 @@ const expect = chai.expect;
 
 describe('header reducer', () => {
   it('handles new tournament submit', () => {
-    const initialState = fromJS();
-    const expectedState = fromJS();
-
-    const action = actions.fillin();
+    const initialState = fromJS(SUBMIT_STATE);
+    const expectedState = fromJS(SUBMIT_STATE_NEXT);
+    const tourn = initialState.get('tournament');
+    const action = actions.submitNewTourn(tourn);
 
     const nextState = header(initialState, action);
     expect(nextState).to.equal(expectedState);
   });
 
   it('handles tournament select', () => {
-    const initialState = fromJS();
-    const expectedState = fromJS();
+    const initialState = fromJS(SUBMIT_STATE);
+    const expectedState = fromJS(SUBMIT_STATE_NEXT);
 
-    const action = actions.fillin();
-
-    const nextState = header(initialState, action);
-    expect(nextState).to.equal(expectedState);
-  });
-
-  it('handles alert delete', () => {
-    const initialState = fromJS();
-    const expectedState = fromJS();
-
-    const action = actions.fillin();
+    const action = actions.selectTourn(1);
 
     const nextState = header(initialState, action);
     expect(nextState).to.equal(expectedState);
   });
 
-  it('handles invite accept', () => {
-    const initialState = fromJS();
-    const expectedState = fromJS();
+  it('handles alert deletion', () => {
+    const initialState = fromJS(DELETE_ALERT_STATE);
+    const expectedState = fromJS(DELETE_ALERT_STATE_NEXT);
 
-    const action = actions.fillin();
+    const action = actions.deleteAlert(1);
+
+    const nextState = header(initialState, action);
+    expect(nextState).to.equal(expectedState);
+  });
+
+  it('handles invite acceptance', () => {
+    const initialState = fromJS(ACCEPT_INVITE_STATE);
+    const expectedState = fromJS(ACCEPT_INVITE_STATE_NEXT);
+
+    const action = actions.acceptInvite(1, 0);
 
     const nextState = header(initialState, action);
     expect(nextState).to.equal(expectedState);
   });
 
   it('handles toggle tourn select', () => {
-    const initialState = fromJS();
-    const expectedState = fromJS();
+    const initialState = fromJS(TOGGLE_SELECT_STATE);
+    const expectedState = fromJS(TOGGLE_SELECT_STATE_NEXT);
 
-    const action = actions.fillin();
+    const action = actions.toggleTournSelect();
 
     const nextState = header(initialState, action);
     expect(nextState).to.equal(expectedState);
   });
 
   it('handles toggle alerts', () => {
-    const initialState = fromJS();
-    const expectedState = fromJS();
+    const initialState = fromJS(TOGGLE_ALERT_STATE);
+    const expectedState = fromJS(TOGGLE_ALERT_STATE_NEXT);
 
-    const action = actions.fillin();
+    const action = actions.toggleAlerts();
 
     const nextState = header(initialState, action);
     expect(nextState).to.equal(expectedState);
