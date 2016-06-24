1bdf9783fb72128ae161f120719b348b6e247f27
Maher Dakkak <madakkak@gmail.com>
2016-06-24 10:33:43 -0500

    Begin implementing and testing chat reducer.

--- client/src/reducers/chat.jsx
+++ client/src/reducers/chat.jsx
@@ -2,18 +2,24 @@
 // Chat reducer
 //
 
-import INITIAL_STATE from '../../../data/state.jsx';
 import { fromJS } from 'immutable';
 
-function handleChatUpdate(state, chat) {
+function handleSubmitChat(state, chat) {
+  return state.set('chat', chat);
+}
+
+function handleChatUpdate(state, chatHist) {
   return state.set('chat', chat);
 }
 
 export default function chat(state = {}, action) {
   switch (action.type) {
+    case 'SUBMIT_CHAT':
+      return handleSubmitChat(state.tournament, action.chat);
     case 'UPDATE_CHAT':
-      return handleChatUpdate(state.tournament, action.chat);
+      return handleChatUpdate(state.tournament, action.chatHist);
     default:
       return state;
   }
 }
+
