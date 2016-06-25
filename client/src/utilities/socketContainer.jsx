513bbf81b02888c691d8284764600cb0fdc2c28f
Maher Dakkak <madakkak@gmail.com>
2016-06-25 11:21:22 -0500

    Fix duplicate server socket connection events. Also modify client socket config to explicitly call io.connect and output connection log messages."

--- client/src/utilities/socketContainer.jsx
+++ client/src/utilities/socketContainer.jsx
@@ -1,14 +1,30 @@
 import io from 'socket.io-client';
 
 let socket;
-
+const connectionString = `${location.protocol}//${location.hostname}${location.port === 80 ?
+                         '' : `:${location.port}`}`;
 if (process.title && process.title != 'browser') {
   socket = {};
 } else {
   console.log('Socket canary');
-  socket = io(
-    `${location.protocol}//${location.hostname}${location.port === 80 ? '' : `:${location.port}`}`
-  );
+  socket = io.connect(connectionString, {
+    reconnectionAttempts: 3,
+  });
 }
+
+/**
+ *  Configure socket connection settings here.
+**/
+socket.on('connect', () => {
+  console.log('Client socket connected');
+});
+
+socket.on('connect_error', error => {
+  console.log('Client socket connection error:', error);
+});
+
+socket.on('connect_timeout', data => {
+  console.log('Socket connection attempt timed out:', data);
+});
 
 export { socket };
