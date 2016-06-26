c8d3ae7255c58f9b15cc403d342e761a7eeda2a7
SpartanSOS <markb539@gmail.com>
2016-06-25 19:18:12 -0500

    Implemented alert list and deletion, user tourn list, refactored create tournament, added props to Header component

--- client/src/components/header/userTournaments.jsx
+++ client/src/components/header/userTournaments.jsx
@@ -1,25 +1,45 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes }from 'react';
 import '../../styles/header_styles.css';
 
-
-
-
-
-const UserTournaments = ({ showTournList, userTourns, toggleTournSelect }) => (
-  showTournList ?
-  <div>
-    <button className="dropdownBtn" onClick={toggleTournSelect}>Tournaments List</button>
-    <ol className='tournDropdownList'>
-      {userTourns.map(function(tournament){
-        return <li key={tournament.get('tournId')}>{tournament.get('tournName')}</li>
-      })}
-    </ol>
-  </div>
-  :
-  <div>
-    <button className="dropdownBtn" onClick={toggleTournSelect}>Tournaments List</button>
-  </div>
-)
-
+const UserTournaments = (props) => (
+  props.showTournList ?
+    <div>
+      <button
+        className="dropdownBtn"
+        onClick={props.toggleTournSelect}
+      >
+        Tournaments List
+      </button>
+      <ul className="tournDropdownList">
+        {props.userTourns.map(tournament =>
+          <TournListItem
+            key={tournament.get('tournId')}
+            tournament={tournament}
+            onTournamentClick={props.selectTourn}
+          />
+        )}
+      </ul>
+    </div>
+    :
+    <div>
+      <button
+        className="dropdownBtn"
+        onClick={props.toggleTournSelect}
+      >
+        Tournaments List
+      </button>
+    </div>
+);
 
 export default UserTournaments;
+
+const TournListItem = (props) => {
+  let _onClick = () => (
+    props.ontournamentClick(props.tournament.get('tournId'))
+  )
+  return (
+    <li onClick={_onClick}>
+      {props.tournament.get('tournName')}
+    </li>
+  );
+};
