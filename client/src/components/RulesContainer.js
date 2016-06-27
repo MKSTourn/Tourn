d082bf909f41d46fab2364d72139e10b9d0fc075
SpartanSOS <markb539@gmail.com>
2016-06-27 12:46:06 -0500

    Implemented rules component and reducer

--- client/src/components/RulesContainer.js
+++ client/src/components/RulesContainer.js
@@ -4,15 +4,17 @@ import * as actionCreators from '../actions/action_creators.jsx';
 import Rules from './rules/Rules.jsx';
 
 
-function mapStateToProps(state){
-  console.log('mapStateToProps showTournList', state.getIn(['header','showTournList']));
-  console.log('mapStateToProps userTourns', state.getIn(['header', 'userData', 'userTourns']));
+function mapStateToProps(state) {
+  console.log('mapStateToProps RulesContainer= ', state.toJS());
   return {
-
-  }
+    mode: state.get('mode'),
+    isOrganizer: state.getIn(['tournament', 'info', 'isOrganizer']),
+    rules: state.getIn(['tournament', 'info', 'rules']),
+    tournType: state.getIn(['tournament', 'info', 'tournType']),
+  };
 }
 
-function mapDispatchToProps(dispatch){
+function mapDispatchToProps(dispatch) {
   return bindActionCreators(actionCreators, dispatch);
 }
 
--- client/src/components/rules/Rules.jsx
+++ client/src/components/rules/Rules.jsx
@@ -4,11 +4,49 @@ import * as actions from '../../actions/action_creators.jsx';
 import { connect } from 'react-redux';
 import { Grid, Row, Col } from 'react-bootstrap';
 
-const Rules = () => (
-  <footer>
-    <p>Rules box is rendering</p>
-  </footer>
-);
+const Rules = (props) => {
+  if (props.mode === 'Edit') {
+    let _textChange = (e) => {
+      let textValue = e.target.value;
+      return props.updateRules(textValue);
+    }
+    return (
+      <footer>
+        <span>
+          <label>Tournament Type: <br></br>
+            <button onClick={props.updateType.bind(null, 'single')}>Single Elimination</button><br></br>
+            <button onClick={props.updateType.bind(null, 'double')}>Double Elimination</button><br></br>
+            <button onClick={props.updateType.bind(null, 'roundRobbin')}>Round Robbin</button>
+          </label><br></br>
+          <label>
+            Rules: <br></br>
+            <input
+              onChange={_textChange}
+              type="text"
+              placeholder="Type Your Tourn Rules Here"
+            >
+            </input>
+          </label>
+        </span>
+      </footer>
+    );
+  } else {
+    return (
+      <footer>
+        <span>
+          <label>Tournament Type: <br></br>
+            <span>{props.tournType}</span>
+          </label><br></br>
+          <label>
+            Rules: <br></br>
+            <span>{props.rules}</span>
+          </label>
+        </span>
+      </footer>
+    );
+  }
+};
+
 
 
 export default Rules;
