3c29b87471d16ee22b6e9deabbd178aae49e4105
Mark Boraski <SpartanSOS@users.noreply.github.com>
2016-06-27 13:24:04 -0500

    Implemented rules component and reducer (#65)

--- client/src/components/RulesContainer.js
+++ client/src/components/RulesContainer.js
@@ -3,14 +3,17 @@ import { connect } from 'react-redux';
 import * as actionCreators from '../actions/action_creators.jsx';
 import Rules from './rules/Rules.jsx';
 
-
-function mapStateToProps(state){
+function mapStateToProps(state) {
+  console.log('mapStateToProps RulesContainer= ', state.toJS());
   return {
-
-  }
+    mode: state.get('mode'),
+    isOrganizer: state.getIn(['tournament', 'info', 'isOrganizer']),
+    rules: state.getIn(['tournament', 'info', 'rules']),
+    tournType: state.getIn(['tournament', 'info', 'tournType']),
+  };
 }
 
-function mapDispatchToProps(dispatch){
+function mapDispatchToProps(dispatch) {
   return bindActionCreators(actionCreators, dispatch);
 }
 
