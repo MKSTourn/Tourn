5709a769bcd9d2ac15559f18144ebee5d28c40d8
SpartanSOS <markb539@gmail.com>
2016-07-04 21:21:20 -0500

    Implemented chat change

--- client/src/components/chat/ChatMessage.jsx
+++ client/src/components/chat/ChatMessage.jsx
@@ -3,22 +3,26 @@ import '../../../public/assets/styles/main.css';
 
 const ChatMessage = (props) => (
   props.currentUser === props.sender ?
-  <li className="transparent currentSender">
-    <div className="testimonials-v4 currentSender">
-      <div className="testimonials-v4-in">
-        <label className=""> {(props.currentUser === props.sender).toString()} <span className="testimonials-author">{props.timeStamp}</span></label>
-      </div>
-      <img className="rounded-x currentSender" src={props.img} alt={props.sender}></img>
-    </div>
+  <li className="transparent">
+    <span className="testimonials-v4">
+      <article className="testimonials-v4-in picLeft">
+        <p className=""> {props.message} <span className="testimonials-author">{props.timeStamp}</span></p>
+      </article>
+      <aside className="picRight">
+        <img className="rounded-x" src={props.img} alt={props.sender}></img>
+      </aside>
+    </span>
   </li>
   :
   <li className="transparent">
-    <div className="testimonials-v4">
-      <div className="testimonials-v4-in">
-        <label className=""> {props.message} <span className="testimonials-author">{props.timeStamp}</span></label>
-      </div>
-      <img className="rounded-x" src={props.img} alt={props.sender}></img>
-    </div>
+    <span className="testimonials-v4">
+      <article className="testimonials-v4-in picRight">
+        <p className=""> {props.message} <span className="testimonials-author">{props.timeStamp}</span></p>
+      </article>
+      <aside className="picLeft">
+        <img className="rounded-x" src={props.img} alt={props.sender}></img>
+      </aside>
+    </span>
   </li>
 );
 
